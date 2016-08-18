233a140b1f2750586c693a670bfbd8b626e8dc60
lidymao <857323309@qq.com>
2016-08-18 09:17:43 +0800

    Create UserInfo.java

---
+++ src/org/dhgy/gypt/longin/entity/UserInfo.java
@@ -0,0 +1,61 @@
+package org.dhgy.gypt.entity;
+
+
+/**
+ * @
+ * @author lidy 20160801
+ * 
+ */
+public class UserInfo {
+	private int userId;
+	private String userName;
+	private String password;
+	private String email;
+	private int qq;
+	private int phone;
+	private int notes;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getQq() {
+		return qq;
+	}
+	public void setQq(int qq) {
+		this.qq = qq;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public int getNotes() {
+		return notes;
+	}
+	public void setNotes(int notes) {
+		this.notes = notes;
+	}
+	
+	
+}
